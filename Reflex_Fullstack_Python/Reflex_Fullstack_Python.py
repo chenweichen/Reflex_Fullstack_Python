c43c85c09d9ff4532ae6c1bf098113e37520e60e
Weichen Chen <matthew.chen.office@gmail.com>
2025-01-16 15:53:41 +0800

    Dynamic Content and on_click Events

--- Reflex_Fullstack_Python/Reflex_Fullstack_Python.py
+++ Reflex_Fullstack_Python/Reflex_Fullstack_Python.py
@@ -7,65 +7,69 @@
 #NOTE - Youtube Video Tutorial
 # [Build Full Stack Web Apps in Pure Python with Reflex - No Javascript Required](https://www.youtube.com/watch?v=ITOZkzjtjUA)
 
-#import reflex as rx
-#
-#from rxconfig import config
-#
-#
-#class State(rx.State):
-#    """The app state."""
-#
-#    ...
-#
-#
-#def index() -> rx.Component:
-#    # Welcome Page (Index)
-#    return rx.container(
-#        rx.color_mode.button(position="top-right"),
-#        rx.vstack(
-#            rx.heading("Welcome to Reflex!", size="9"),
-#            rx.text(
-#                "Get started by editing ",
-#                rx.code(f"{config.app_name}/{config.app_name}.py"),
-#                size="5",
-#            ),
-#            rx.link(
-#                rx.button("Check out our docs!"),
-#                href="https://reflex.dev/docs/getting-started/introduction/",
-#                is_external=True,
-#            ),
-#            spacing="5",
-#            justify="center",
-#            min_height="85vh",
-#        ),
-#        rx.logo(),
-#    )
-#
-#
-#app = rx.App()
-#app.add_page(index)
-
-#############################################################
-
-
 import reflex as rx
 
 from rxconfig import config
 
 
+class State(rx.State):
+    """The app state."""
+    label="This is my label."
+    original_label = "This is my label."
+
+    def change_label(self):
+        if self.label == "Cool label.":
+            self.label = self.original_label
+        else:
+            self.label = "Cool label."
+
+
 def index() -> rx.Component:
-    return rx.text(f"Hello, World")
-
-
-app = rx.App(
-    theme=rx.theme(
-        appearance="dark",
-        has_background=True,
-        panel_background="solid",
-        scaling="90%",
-        radius="medium",
-        accent_color="sky",
+    # Welcome Page (Index)
+    return rx.container(
+        rx.color_mode.button(position="top-right"),
+        rx.vstack(
+            rx.heading(State.label, " Welcome to Reflex!", size="9"),
+            rx.text(
+                "Get started by editing ",
+                rx.code(f"{config.app_name}/{config.app_name}.py"),
+                on_click=State.change_label,
+                size="5",
+            ),
+            #rx.button("Change label content.", on_click=State.change_label),
+            rx.link(
+                rx.button("Check out our docs!"),
+                href="https://reflex.dev/docs/getting-started/introduction/",
+                is_external=True,
+            ),
+            spacing="5",
+            justify="center",
+            min_height="85vh",
+        ),
+        rx.logo(),
     )
-)
 
+
+app = rx.App()
 app.add_page(index)
+
+#############################################################
+
+
+#import reflex as rx
+#from rxconfig import config
+#
+#def index() -> rx.Component:
+#    return rx.text(f"Hello, World")
+#
+#app = rx.App(
+#    theme=rx.theme(
+#        appearance="dark",
+#        has_background=True,
+#        panel_background="solid",
+#        scaling="90%",
+#        radius="medium",
+#        accent_color="sky",
+#    )
+#)
+#app.add_page(index)
