38a8b5f2fb9b60ecf5accfefcd3e37690c5040da
Weichen Chen <matthew.chen.office@gmail.com>
2025-01-17 15:04:20 +0800

    Add Feature: when I click a heading, below the heading shows an input? After I type something in input, the change shows in heading and input as well. After that, I finished the typing, the input disspears.

--- Reflex_Fullstack_Python/Reflex_Fullstack_Python.py
+++ Reflex_Fullstack_Python/Reflex_Fullstack_Python.py
@@ -7,6 +7,37 @@
 #NOTE - Youtube Video Tutorial
 # [Build Full Stack Web Apps in Pure Python with Reflex - No Javascript Required](https://www.youtube.com/watch?v=ITOZkzjtjUA)
 
+
+#NOTE - 
+# rx.input(
+        value=f"{cls.expected_quantity}",
+        on_change=cls.set_expected_quantity,
+        placeholder="請輸入預期數量",
+        type="number",
+# ), # rx.input
+
+
+#@rx.event
+#    def set_expected_quantity(self, expect_quantity: str):
+#        # DeprecationWarning: Mismatched event handler argument types has been deprecated in version 0.6.5 Event handler on_change expects <class 'str'> for argument expect_quantity but got <class 'int'>
+#        #self.expected_quantity = expect_quantity
+#        if expect_quantity.strip() == "":
+#            self.expected_quantity = 0
+#        else:
+#            try:
+#                self.expected_quantity = int(expect_quantity)
+#            except ValueError:
+#                self.expected_quantity = 0
+
+
+# References:
+#input component event handler #1719
+#https://github.com/orgs/reflex-dev/discussions/1719
+#https://reflex.dev/docs/events/setters/
+
+
+
+
 import reflex as rx
 
 from rxconfig import config
@@ -16,19 +47,25 @@ class State(rx.State):
     """The app state."""
     label="This is my label."
 
+    @rx.event
+    def handle_title_input_change(self, val):
+        self.label = val
+
+    ################
+
     text: str = "Click me to edit"
     show_input: bool =False
 
+    @rx.event
     def toggle_input(self):
         self.show_input = not self.show_input
 
     @rx.event
-    def handle_enter(self, value: str):
+    def set_text(self, value: str):
         self.text = value
-        self.show_input = False
 
-    def handle_title_input_change(self, val):
-        self.label = val
+    ################
+    
     
     #def handle_heading_change(self, )
 
@@ -45,6 +82,8 @@ def index() -> rx.Component:
                 size="5",
             ),
             rx.input(on_change=State.handle_title_input_change), # Here on_change is trigger
+            
+            ################
             rx.heading(
                 State.text,
                 on_click=State.toggle_input,
@@ -53,9 +92,12 @@ def index() -> rx.Component:
                 State.show_input,
                 rx.input(
                     value=State.text,
-                    on_blur=State.handle_enter,
+                    on_change=State.set_text,
+                    on_blur=State.toggle_input,
                 )
             ),
+            ################
+
             rx.link(
                 rx.button("Check out our docs!"),
                 href="https://reflex.dev/docs/getting-started/introduction/",
